129fe643a3c33de401cc6f49ff7684349eceba58
User <theodoraaurellita2023@gmail.com>
2024-02-16 13:22:24 +0700

    push button,button group

---
+++ fuzzy1.m
@@ -0,0 +1,80 @@
+function varargout = fuzzy1(varargin)
+% FUZZY1 MATLAB code for fuzzy1.fig
+%      FUZZY1, by itself, creates a new FUZZY1 or raises the existing
+%      singleton*.
+%
+%      H = FUZZY1 returns the handle to a new FUZZY1 or the handle to
+%      the existing singleton*.
+%
+%      FUZZY1('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in FUZZY1.M with the given input arguments.
+%
+%      FUZZY1('Property','Value',...) creates a new FUZZY1 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before fuzzy1_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to fuzzy1_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help fuzzy1
+
+% Last Modified by GUIDE v2.5 16-Feb-2024 13:18:34
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @fuzzy1_OpeningFcn, ...
+                   'gui_OutputFcn',  @fuzzy1_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before fuzzy1 is made visible.
+function fuzzy1_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to fuzzy1 (see VARARGIN)
+
+% Choose default command line output for fuzzy1
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes fuzzy1 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = fuzzy1_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
